9c64d3574c0b55493c228e272814d5b6b6e71952
Victor Mendele <victor.mendele68@gmail.com>
2019-02-15 17:15:49 +0100

    feat: htaccess

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    backendRoot: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://stark-cove-46870.herokuapp.com/'
+    backendRoot: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://stark-cove-46870.herokuapp.com'
 }
