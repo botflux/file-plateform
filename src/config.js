1a58c1e8e51e3290d0650347e9fc2fa4b0621dd9
Victor Mendele <victor.mendele68@gmail.com>
2019-02-15 16:17:02 +0100

    feat: conf file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export default {
-    backendRoot: 'http://localhost:3000'
+    backendRoot: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://stark-cove-46870.herokuapp.com/'
 }
