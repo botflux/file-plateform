c5d911504aa7f4c72aa5da60775065d393662bec
Victor Mendele <victor.mendele68@gmail.com>
2019-02-19 17:18:38 +0100

    Revert "feat: csv to xml route"
    This reverts commit 4140990652e08073c5919e0fc114041efcd27e78.

--- src/views/csv-to-xml/start.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="container d-flex justify-content-center">
-        <router-link :to="{ name: 'character-checker' }" class="btn btn-success">Commencer la conversion</router-link>
-    </div>
-</template>
