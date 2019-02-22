5ad3c13ed3f8bf58b4997a01a8ccef409dd6ecac
Victor Mendele <victor.mendele68@gmail.com>
2019-02-22 17:41:04 +0100

    fix

--- src/views/csv-to-xml/download.vue
+++ src/views/csv-to-xml/download.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="d-flex justify-content-center">
         <a :href="downloadUrl" download class="btn btn-primary">Télécharger le XML</a>
     </div>
 </template>
