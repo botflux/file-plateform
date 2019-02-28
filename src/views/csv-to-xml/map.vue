08dfc094f394d1573b56898f3cf13164d04ce54b
Victor Mendele <victor.mendele68@gmail.com>
2019-02-28 14:48:26 +0100

    fix: cleaning map view

--- src/views/csv-to-xml/map.vue
+++ src/views/csv-to-xml/map.vue
@@ -13,40 +13,10 @@
         </div>
         <div class="row mt-5">
             <div class="col-md-6">
-                <!-- <div class="d-flex flex-column">
-                    <h3 class="mb-3">Définition des champs du XML</h3>
-                    <button class="btn btn-success mr-auto mb-3" @click="addMapField()">Ajouter un champs</button>
-                    <div class="d-flex align-items-start py-3 border-bottom border-top" v-for="(mapField, i) in $v.mapFields.$each.$iter" :key="`map-field-${i}`">
-                        <map-field :field="mapField" :field-id="Number.parseInt(i)" />
-                        <button class="btn btn-danger ml-3" @click="deleteMapField(i)">Supprimer</button>
-                    </div>
-                </div> -->
-
                 <map-field-container></map-field-container>
-
                 <xml-tags></xml-tags>
-
                 <xml-attribute-container></xml-attribute-container>
-
-                <!-- <div class="d-flex flex-column mt-5">
-                    <h3 class="mb-3">Définition des attributs de l'élément global</h3>
-                    <button class="btn btn-success mr-auto mb-4" @click="addAttribute()">Ajouter un attribut</button>
-                    <div class="py-3 border-bottom border-top" v-for="(attribute, i) in attributes" :key="`attribute-${i}`">
-                        <xml-attribute :attribute="attribute" :attributeId="i" />
-                        <button class="btn btn-danger" @click="removeAttribute(i)">Supprimer</button>
-                    </div>
-                </div> -->
-
                 <xml-declaration-container></xml-declaration-container>
-
-                <!-- <div class="d-flex flex-column mt-5">
-                    <h3 class="mb-3">Définition des déclarations du fichier XML</h3>
-                    <button class="btn btn-success mr-auto mb-3" @click="addDeclaration()">Ajouter une déclaration</button>
-                    <div class="py-3 border-bottom border-top" v-for="(declaration, i) in declarations" :key="`declaration-${i}`">
-                        <xml-declaration :declaration="declaration" :declarationId="i" />
-                        <button class="btn btn-danger" @click="removeDeclaration(i)">Supprimer</button>
-                    </div>
-                </div> -->
             </div>
             <div class="col-md-6">
                 <xml-preview class="sticky-top my-5"></xml-preview>
@@ -57,18 +27,17 @@
 
 <script>
 import { createNamespacedHelpers } from 'vuex'
-import * as types from '@/stores/types'
+
 import CSVHeader from '@/components/CSVHeader'
 import CSVHeadersContainer from '@/components/CSVHeadersContainer'
+
 import XMLPreview from '@/components/XMLPreview'
 import XMLDeclarationContainer from '@/components/XMLDeclarationContainer'
-// import config from '@/config.js'
 import MapFieldContainer from '@/components/MapFieldContainer'
 import XMLAttributeContainer from '@/components/XMLAttributeContainer.vue'
 import XMLTags from '@/components/XMLTags'
 
-// eslint-disable-next-line
-const { mapGetters, mapState, mapActions } = createNamespacedHelpers('csvToXml')
+const { mapState } = createNamespacedHelpers('csvToXml')
 
 export default {
     components: {
@@ -86,53 +55,6 @@ export default {
             file: state => state.file,
             fields: state => state.fields
         }),
-        ...mapGetters([
-            types.CSV_TO_XML_FILE_IS_VALID,
-            types.CSV_TO_XML_HEADERS_ARE_VALID,
-            types.CSV_TO_XML_FIELDS_ARE_VALID,
-            types.CSV_TO_XML_GLOBAL_TAG_IS_VALID,
-            types.CSV_TO_XML_COLLECTION_TAG_IS_VALID,
-            types.CSV_TO_XML_DECLARATIONS_ARE_VALID,
-            types.CSV_TO_XML_ATTRIBUTES_ARE_VALID,
-        ]),
-        dataAreValid () {
-            return (
-                this[types.CSV_TO_XML_FIELDS_ARE_VALID] &&
-                this[types.CSV_TO_XML_GLOBAL_TAG_IS_VALID] &&
-                this[types.CSV_TO_XML_COLLECTION_TAG_IS_VALID] &&
-                this[types.CSV_TO_XML_DECLARATIONS_ARE_VALID] &&
-                this[types.CSV_TO_XML_ATTRIBUTES_ARE_VALID]
-            )
-        }
-    },
-    filters: {
-        surround (value) {
-            if (!value) return ''
-            return `<${value}>`
-        },
-        addSlash (value) {
-            if (!value) return ''
-            return `/${value}`
-        }
-    },
-    methods: {
-        addAttribute () {
-            this.attributes.push({
-                name: '',
-                value: ''
-            })
-        },
-        removeAttribute (index) {
-            this.attributes = this.attributes.filter ((a, i) => i != index)
-        },
-        ...mapActions([
-            types.SET_CSV_TO_XML_GLOBAL_TAG,
-            types.SET_CSV_TO_XML_COLLECTION_TAG,
-            types.SET_CSV_TO_XML_ATTRIBUTES,
-            types.SET_CSV_TO_XML_DECLARATIONS,
-            types.SET_CSV_TO_XML_FIELDS,
-            types.SET_CSV_TO_XML_DOWNLOAD
-        ])
     },
     /* eslinl-disable */
     // beforeRouteEnter (to, from, next) {
